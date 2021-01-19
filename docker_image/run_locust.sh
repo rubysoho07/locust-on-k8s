f5bf0dfef60ffb684bd856db1117d0968e3d46d2
Yungon Park <hahafree12@gmail.com>
2021-01-19 22:10:32 +0900

    Add Dockerfile and script to build Docker Image

---
+++ docker_image/run_locust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download Locustfile from S3
+if [ -z $S3_ENDPOINT ]; then
+    aws s3 cp s3://$S3_BUCKET/$S3_KEY locustfile.py
+else
+    # For S3 Compatible Storage
+    aws --endpoint-url $S3_ENDPOINT s3 cp s3://$S3_BUCKET/$S3_KEY locustfile 
+fi
+
+# Master / Worker mode
+if [ -z $LOCUST_MODE ]; then
+    locust
+elif [ $LOCUST_MODE == "master" ]; then
+    locust --master
+else
+    locust --worker --master-host $LOCUST_MASTER_HOST --master-port $LOCUST_MASTER_PORT
+fi
